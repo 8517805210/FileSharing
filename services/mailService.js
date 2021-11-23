37886a56c1b899fdcf1d00a306b3575e472625e4
Mohit <mohitkumar11742@gmail.com>
2021-11-23 07:52:22 +0530

    changed email sending third party support through sendblue in mailService.js

--- services/mailService.js
+++ services/mailService.js
@@ -1,25 +1,48 @@
 const nodemailer = require("nodemailer");
-const sgMail = require('@sendgrid/mail');
+module.exports = async ({ from, to, subject, text, html}) => {
+        let transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.MAIL_USER, // generated ethereal user
+                pass: process.env.MAIL_PASSWORD, // generated ethereal password
+            },
+        });
 
-//--
-const apiKey = process.env.API_KEY;
-sgMail.setApiKey(apiKey);
-//--
-module.exports =  ({ from, to, subject, text, html}) => {
+        // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: `inShare <${from}>`, // sender address
+        to: to, // list of receivers
+        subject: subject, // Subject line
+        text: text, // plain text body
+        html: html, // html body
+    });
+}
 
-    //--
-    const msg = {
-        to,from,subject,text,html
-      }
 
-  sgMail.send(msg).then(() => {
-    console.log('Email sent')
-  }).catch((error) => {
-    console.error(error)
-  })
+// const nodemailer = require("nodemailer");
+// const sgMail = require('@sendgrid/mail');
+
+// //--
+// const apiKey = process.env.API_KEY;
+// sgMail.setApiKey(apiKey);
+// //--
+// module.exports =  ({ from, to, subject, text, html}) => {
+
+//     //--
+//     const msg = {
+//         to,from,subject,text,html
+//       }
+
+//   sgMail.send(msg).then(() => {
+//     console.log('Email sent')
+//   }).catch((error) => {
+//     console.error(error)
+//   })
 
    
-}
+// }
 
 
 
